15d0912307cdfec006d858dcd1dce6f98f17b2a4
Jörn Heinemann <joernheinemann@gmx.de>
2020-08-30 12:05:58 +0200

    helper die route maps erstellen hinzugefügt

--- test/helper.ts
+++ test/helper.ts
@@ -1,5 +1,6 @@
 import {RouterOptions} from "../src/router";
 import RouteCollectorInterface from "../src/Interfaces/RouteCollectorInterface";
+import DispatcherInterface from "../src/Interfaces/DispatcherInterface";
 
 export function createNewRouteCollector(options: RouterOptions = {}): RouteCollectorInterface
 {
@@ -24,7 +25,8 @@ export function createNewRouteCollector(options: RouterOptions = {}): RouteColle
 	return new routeCollectorClass.default(new generator.default());
 }
 
-export function createNewDispatcher(collector: RouteCollectorInterface, options: RouterOptions = {}) {
+export function createNewDispatcher(collector: RouteCollectorInterface, options: RouterOptions = {}): DispatcherInterface
+{
 	let dispatcherPath = "";
 
 	if(!options.dispatcher) {
@@ -36,4 +38,175 @@ export function createNewDispatcher(collector: RouteCollectorInterface, options:
 	let dispatcherClass = require(dispatcherPath);
 
 	return new dispatcherClass.default(collector.getData());
+}
+
+export function createBigMap(r: RouteCollectorInterface)
+{
+	r.get("/",() => {
+		return "index";
+	});
+
+	r.group("/test1",() => {
+		//dynamic routes test
+		r.group("/:id",() => {
+			r.get("",() => {
+				return "matched GET dynamic";
+			});
+
+			r.post("",() => {
+				return "matched POST dynamic";
+			});
+
+			r.put("",() => {
+				return "matched PUT dynamic";
+			});
+
+			r.delete("",() => {
+				return "matched DELETE dynamic";
+			});
+
+			r.options("",() => {
+				return "matched OPTIONS dynamic";
+			});
+
+			r.patch("",() => {
+				return "matched PATCH dynamic";
+			});
+
+			r.head("",() => {
+				return "matched HEAD dynamic";
+			});
+
+			r.any("",() => {
+				return "matched any dynamic";
+			});
+		});
+
+		r.group("/static",() => {
+			r.get("",() => {
+				return "matched GET static";
+			});
+
+			r.post("",() => {
+				return "matched POST static";
+			});
+
+			r.put("",() => {
+				return "matched PUT static";
+			});
+
+			r.delete("",() => {
+				return "matched DELETE static";
+			});
+
+			r.options("",() => {
+				return "matched OPTIONS static";
+			});
+
+			r.patch("",() => {
+				return "matched PATCH static";
+			});
+
+			r.head("",() => {
+				return "matched HEAD static";
+			});
+
+			r.any("",() => {
+				return "matched any static";
+			});
+		});
+
+		//nested groups
+
+		r.group("/nestedGroup1",() => {
+			r.get("/nestedRoute1",() => {
+				return "nestedG1R1";
+			});
+
+			r.get("/nestedRoute2/:id",() => {
+				return "nestedG1R2";
+			});
+
+			r.group("/nestedGroup2",() => {
+				r.get("/nestedRoute1",() => {
+					return "nestedG2R1";
+				});
+
+				r.get("/nestedRoute2/:id",() => {
+					return "nestedG2R2";
+				});
+
+				r.group("/nestedGroup3",() => {
+					r.get("/nestedRoute1",() => {
+						return "nestedG3R1";
+					});
+
+					r.get("/nestedRoute2/:id",() => {
+						return "nestedG3R2";
+					});
+				});
+			});
+		});
+	});
+
+	r.get("/dynamic/:id2/:id3/:id4",() => {
+		return "route with more than one dynamic param";
+	});
+}
+
+export function createMapWithMiddleware(r: RouteCollectorInterface)
+{
+	r.get("/routeMw1",() => {
+
+	}).add(() => {
+		return "routeMw1";
+	});
+
+	r.group("/mwGroup1",() => {
+		r.get("/routeMw2",() => {
+
+		}).add(() => {
+			return "routeMw2";
+		});
+
+		r.group("/mwGroup2",() => {
+			r.group("/mwGroup3",() => {
+				r.get("/routeMw4",() => {
+
+				}).add(() => {
+					return "routeMw4";
+				});
+			}).add(() => {
+				return "mwGroup3";
+			});
+
+			r.get("/routeMw3",() => {
+
+			}).add(() => {
+				return "routeMw3";
+			});
+		}).add(() => {
+			return "mwGroup2";
+		});
+	}).add(() => {
+		return "mwGroup1";
+	});
+}
+
+export function createRouteMap(options: RouterOptions = {}, mapType: number = 1): RouteCollectorInterface
+{
+	const r = createNewRouteCollector(options);
+
+	switch (mapType) {
+		case 1:
+			createBigMap(r);
+			break;
+		case 2:
+			createMapWithMiddleware(r);
+			break;
+		default:
+			break;
+	}
+
+	return r;
 }
