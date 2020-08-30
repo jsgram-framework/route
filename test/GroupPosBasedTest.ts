1f0822f0d56b5f2d529d4ef665baabca22a00e7e
Jörn Heinemann <joernheinemann@gmx.de>
2020-08-30 18:13:02 +0200

    routertest für groupposbased added

--- test/GroupPosBasedTest.ts
+++ test/GroupPosBasedTest.ts
@@ -1,5 +1,11 @@
 import {testDispatcher} from "./DispatcherClass";
+import {routeTest} from "./RouteClass";
+
 
 describe("GroupPosBasedDispatcher",() => {
 	testDispatcher();
+});
+
+describe("GroupPosBasedDispatcherRouteTest",() => {
+	routeTest();
 });
