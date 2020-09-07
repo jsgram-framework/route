9f6619735cf61414932ea472e128945b07021849
Jörn Heinemann <joernheinemann@gmx.de>
2020-09-07 21:05:54 +0200

    test update

--- test/Tree/TreeNormalTest.ts
+++ test/Tree/TreeNormalTest.ts
@@ -42,6 +42,75 @@ describe("TreeNormalTest",() => {
 		evaluateDynamicMatches(params,["param"],["perfectly-fine-route"]);
 	});
 
+	it('should match dynamic route', function () {
+		const r = createNewRouteCollector(options);
+
+		r.get("/foo/:fooParam",() => {
+			return "test";
+		});
+
+		r.get("/foo/bar/:barParam",() => {
+			return "test";
+		});
+
+		r.get("/foo/search",() => {
+			return "test";
+		});
+
+		r.get("/foo/submit",() => {
+			return "test";
+		});
+
+		const d = createNewDispatcher(r,options);
+
+		let [status, routeId, params] = d.dispatch("GET","/foo/awesome-parameter");
+
+		evaluateStaticMatches(0,200,status,routeId);
+		evaluateDynamicMatches(params,["fooParam"],["awesome-parameter"]);
+
+		[status, routeId, params] = d.dispatch("GET","/foo/b-first-character");
+
+		evaluateStaticMatches(0,200,status,routeId);
+		evaluateDynamicMatches(params,["fooParam"],["b-first-character"]);
+
+		[status, routeId, params] = d.dispatch("GET","/foo/s-first-character");
+
+		evaluateStaticMatches(0,200,status,routeId);
+		evaluateDynamicMatches(params,["fooParam"],["s-first-character"]);
+
+		[status, routeId, params] = d.dispatch("GET","/foo/se-prefix");
+
+		evaluateStaticMatches(0,200,status,routeId);
+		evaluateDynamicMatches(params,["fooParam"],["se-prefix"]);
+
+		[status, routeId, params] = d.dispatch("GET","/foo/sx-prefix");
+
+		evaluateStaticMatches(0,200,status,routeId);
+		evaluateDynamicMatches(params,["fooParam"],["sx-prefix"]);
+	});
+
+	it('should match dynamic route with common prefix', function () {
+		const r = createNewRouteCollector(options);
+
+		r.get("/test",() => {
+			return "test";
+		});
+
+		r.get("/:test",() => {
+			return "test";
+		});
+
+		r.get("/test/hello",() => {
+			return "test";
+		});
+
+		const d = createNewDispatcher(r,options);
+
+		let [status, routeId] = d.dispatch("GET","/test");
+
+		evaluateStaticMatches(0,200,status,routeId);
+	});
+
 	it('should not match parametric route without the parameter', function () {
 		const r = createNewRouteCollector(options);
 
